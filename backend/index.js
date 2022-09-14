5ac830af5311365b29829e2310307c1147118b9c
Ognjen <61661017+OgnjenO@users.noreply.github.com>
2022-09-14 21:42:25 +0200

    quickupdate

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5498
 const { WebSocketServer } = require('ws');
-//const wss = new WebSocket.server({ port: 5499 });
-const path = require('path');
-const { createServer } = require('http');
 
-const server = createServer(app);
 const wss = new WebSocketServer({ port: 5499 });
 
 const cors = require('cors');
@@ -16,18 +12,10 @@ let client = null;
 
 wss.on('connection', function (ws) {
   client = ws;
-//  const id = setInterval(function () {
-//    ws.send(JSON.stringify(process.memoryUsage()), function () {
-//      //
-//      // Ignore errors.
-//      //
-//    });
-//  }, 100);
-  console.log('started client interval');
+  console.log('Frontend client connected');
 
   ws.on('close', function () {
-    console.log('stopping client interval');
-    //clearInterval(id);
+    console.log('Frontend client disconnected');
   });
 });
 
@@ -43,9 +31,6 @@ app.get('/keypad/:key', (req, res) => {
   console.log(new Date().toUTCString() + " Keypad: " + key);
   if(client == null) return;
   client.send(JSON.stringify({event: 'keypad', key: key}), function () {
-      //
-      // Ignore errors.
-      //
   });
 });
 
@@ -57,9 +42,6 @@ app.get('/joystick/:y/:x', (req, res) => {
   console.log(new Date().toUTCString() + " Joystick: " + x + "|" + y);
   if(client == null) return;
   client.send(JSON.stringify({ event: 'joystick', x: x, y: y}), function () {
-      //
-      // Ignore errors.
-      //
   });
 });
 
@@ -70,9 +52,6 @@ app.get('/rotencoder/:value', (req, res) => {
   console.log(new Date().toUTCString() + " Rotencoder: " + value);
   if(client == null) return;
   client.send(JSON.stringify({event: 'rotencoder', value: value}), function () {
-      //
-      // Ignore errors.
-      //
   });
 });
 
